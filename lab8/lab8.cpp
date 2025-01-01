4e6ffe237479c3724fa80db1c9a2bf3d7be8e440
Kris <sudoprogdoc@gmail.com>
2025-01-01 15:39:50 +0700

    Add project files.

---
+++ lab8/lab8.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+
+
+int* fill1(int* a)
+{
+    printf("Статический массив, индексная адресация: ");
+    for (int i = 0; i < 10; i++)
+    {
+        a[i] = i * i;
+        printf("%d", a[i]);
+        printf("%s", " ");
+    }
+    printf("\n");
+    return a;
+}
+
+int* fill2(int* a)
+{
+    printf("Статический массив, косвенная адресация: ");
+    int *p;
+    p = a;
+    for (int i = 0; i < 10; i++)
+    {
+        *(p + i) = i * i;
+        printf("%d", *(p + i));
+        printf("%s", " ");
+    }
+    printf("\n");
+    return p;
+}
+
+int* fill3(int* a)
+{
+    printf("Динамический массив, индексная адресация: ");
+    for (int i = 0; i < 10; i++)
+    {
+        a[i] = i * i;
+        printf("%d", a[i]);
+        printf("%s", " ");
+    }
+    printf("\n");
+    return a;
+}
+
+int* fill4(int* a)
+{
+    printf("Динамический массив, косвенная адресация: ");
+    int* p;
+    p = a;
+    for (int i = 0; i < 10; i++)
+    {
+        *(p + i) = i * i;
+        printf("%d", *(p + i));
+        printf("%s", " ");
+    }
+    printf("\n");
+    return p;
+}
+int main()
+{
+    int a1[10];
+    int a2[10];
+    int *a3;
+    a3 = new int[10];
+    int *a4;
+    a4 = new int[10];
+
+    fill1(a1);
+
+    fill2(a2);
+
+    fill3(a3);
+    delete[] a3;
+    
+    fill4(a4);
+    delete[] a4;
+
+    return 0;
+}
